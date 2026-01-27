ee3023f38fa93d23090eb86953bf3d4ebdc26d34
Marcelo da Silva <marceloxhenrique@hotmail.com>
2026-01-27 22:52:26 +0100

    feat: add routes logic

--- router/routes.go
+++ router/routes.go
@@ -8,15 +8,31 @@ import (
 
 func initializeRoutes(router *gin.Engine){
 	v1 := router.Group("/api/v1")
-
 	{
 		v1.GET("/opening", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{
 				"message":"GET Opening",
 			})
 		})
-		
-	
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message":"GET Opening",
+			})
+		})
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message":"GET Opening",
+			})
+		})
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message":"GET Opening",
+			})
+		})
+		v1.GET("/openings", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message":"GET Openings",
+			})
+		})
 	}
-
 }
