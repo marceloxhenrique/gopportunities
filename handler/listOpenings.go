111338c02fe3e80975af7d2510a73920285e5e97
Marcelo da Silva <marceloxhenrique@hotmail.com>
2026-04-21 23:29:15 +0200

    docs: add documentation for deleteOpening, listOpening, showOpening and updateOpening

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -7,6 +7,16 @@ import (
 	"github.com/marceloxhenrique/gopportunities/schemas"
 )
 
+//@BasePath /api/v1
+
+// @Summary List openings
+// @Description Listt all jobs openings
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
 func ListOpeningsHandler(ctx *gin.Context) {
 	openings := []schemas.Opening{}
 	if err := db.Find(&openings).Error; err != nil {
--- handler/response.go
+++ handler/response.go
@@ -32,3 +32,19 @@ type CreateOpeningResponse struct {
 	Message string                  `json:"message"`
 	Data    schemas.OpeningResponse `json:"data"`
 }
+type DeleteOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type ShowOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type UpdateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type ListOpeningResponse struct {
+	Message string                    `json:"message"`
+	Data    []schemas.OpeningResponse `json:"data"`
+}
