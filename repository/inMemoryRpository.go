c27cbf83a4dd8a712fcef9a84ebc92bef83471a8
Marcelo da Silva <marceloxhenrique@hotmail.com>
2026-04-27 12:08:43 +0200

    feat: add inMemoryRepository for opening and createOpeninig test

---
+++ repository/inMemoryRpository.go
@@ -0,0 +1,42 @@
+package repository
+
+import (
+	"errors"
+
+	"github.com/marceloxhenrique/gopportunities/schemas"
+)
+
+type InMemoryOpeningRepository struct {
+	Data   map[uint]*schemas.Opening
+	NextID uint
+}
+
+func NewInMemoryRepository() *InMemoryOpeningRepository {
+	return &InMemoryOpeningRepository{
+		Data: make(map[uint]*schemas.Opening),
+	}
+}
+
+func (r *InMemoryOpeningRepository) Create(opening *schemas.Opening) error {
+	r.NextID++
+	opening.ID = r.NextID
+	r.Data[opening.ID] = opening
+	return nil
+}
+
+func (r *InMemoryOpeningRepository) GetById(id uint) (*schemas.Opening, error) {
+	if o, ok := r.Data[id]; ok {
+		return o, nil
+	}
+	return nil, errors.New("not found")
+}
+
+func (r *InMemoryOpeningRepository) List() ([]schemas.Opening, error) {
+	return nil, nil
+}
+func (r *InMemoryOpeningRepository) Update(opening *schemas.Opening) (*schemas.Opening, error) {
+	return nil, nil
+}
+func (r *InMemoryOpeningRepository) Delete(id uint) error {
+	return nil
+}
